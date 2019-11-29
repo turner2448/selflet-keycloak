26235c0571640095882a5cb1cb0eb9a0d8019ad0
turner2448 <tony.turner@vocalink.com>
2019-11-29 16:29:24 +0000

    changes to kcadm script

--- configure-keycloak.sh
+++ configure-keycloak.sh
@@ -7,10 +7,12 @@ echo "creating selflet"
 echo "creating client"
 ./kcadm.sh create clients -r selflet -s clientId=selflet-client -s enabled=true -s publicClient=true -s clientAuthenticatorType=client-secret -s secret=d0b8122f-8dfb-46b7-b68a-f5cc4e25d000 -s "redirectUris=[\"http://localhost:3000/*\"]" -s baseUrl=http://localhost:3000 -s adminUrl=http://localhost:3000
 echo "creating user"
-./kcadm.sh create users -r selflet -s username=test -s enabled=true  #'requiredActions=["UPDATE_PASSWORD"]'
+./kcadm.sh create users -r selflet -s username=test -s enabled=true -s "attributes.agencyId=1" -s "attributes.agency=true" -s "attributes.tenant=false" -s "attributes.landlord=false" #'requiredActions=["UPDATE_PASSWORD"]'
 ./kcadm.sh set-password -r selflet --username test --new-password password --temporary
+
 echo "Creating Group Admin group"
 ./kcadm.sh create groups -r selflet -s name=Agency_Admin
 json=$(./kcadm.sh get groups -r selflet)
-echo $json
+echo $json | jq '.[] | select(.name == "Agency_Admin") | .id'
 
+echo "Adding test user to group Agency_Admin"
